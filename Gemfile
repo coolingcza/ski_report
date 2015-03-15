fdcfe41a395e5c7e2447e9b946e21096c129ca1e
Crystal Cooling <cooling.cne@gmail.com>
2015-03-15 15:48:55 -0500

    saved db, added pg to Gemfile for prod, limited sqlite3 to dev, test

--- Gemfile
+++ Gemfile
@@ -1,10 +1,11 @@
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
 gem "forecast_io"
 gem "faraday"
 gem "pry"
-gem "sqlite3"
+gem "sqlite3", :groups => [:development, :test]
+gem "pg", :groups => [:production]
 gem "sinatra"
 gem "chartkick"
 gem "sinatra-activerecord"
-gem 'bcrypt', '~> 3.1.10'
+gem "bcrypt", "~> 3.1.10"
--- database/database_setup.rb
+++ database/database_setup.rb
@@ -25,8 +25,8 @@ DATABASE.execute("CREATE TABLE IF NOT EXISTS resorts
                   (id INTEGER PRIMARY KEY, name TEXT, latitude FLOAT, 
                   longitude FLOAT, state TEXT)")
                   
-DATABASE.execute("CREATE TABLE IF NOT EXISTS markers
-                  (id INTEGER PRIMARY KEY, condition TEXT, description TEXT)")
+# DATABASE.execute("CREATE TABLE IF NOT EXISTS markers
+#                   (id INTEGER PRIMARY KEY, condition TEXT, description TEXT)")
                   
 DATABASE.execute("CREATE TABLE IF NOT EXISTS users_resorts
                   (user_id NUMBER, resort_id NUMBER)")
